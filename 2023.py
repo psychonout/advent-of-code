470b84e89ba07ce5d5699734370be8b740150003
Donatas <donatas@gostautas.com>
2023-12-02 12:24:36 +0200

    day two

--- 2023.py
+++ 2023.py
@@ -1,5 +1,6 @@
-from operator import index
-from pprint import pprint
+from pprint import pformat
+
+from loguru import logger
 
 from common import download_input, from_file, get_input
 
@@ -50,27 +51,85 @@ def day_1():
         encoding_keys = []
 
         for line in data:
-            print(line)
             min_occurance = len(line)
             max_occurance = 0
             first_digit = ""
             second_digit = ""
             for digit in digits.keys():
                 try:
-                    line.index(str(digit))
-                    min_occurance = min(min_occurance, line.index(digit))
-                    max_occurance = max(max_occurance, line.rindex(digit))
-                    first_digit = digit
-                    second_digit = digit
+                    logger.debug(f"{digit} in {line} at {line.index(digit)}")
+                    if line.index(digit) < min_occurance:
+                        min_occurance = min(min_occurance, line.index(digit))
+                        first_digit = digit
+                    if line.rindex(digit) >= max_occurance:
+                        max_occurance = max(max_occurance, line.rindex(digit))
+                        second_digit = digit
                 except ValueError:
                     continue
-            print(first_digit)
-            print(second_digit)
-            print(int(f"{digits[first_digit]}{digits[second_digit]}"))
-            break
+            logger.debug(first_digit)
+            logger.debug(second_digit)
+            logger.debug(int(f"{digits[first_digit]}{digits[second_digit]}"))
+            encoding_keys.append(int(f"{digits[first_digit]}{digits[second_digit]}"))
+        return sum(encoding_keys)
 
     return part_two()
 
 
+def day_2():
+    data = download_input(2, 2023).splitlines()
+
+    def part_one():
+        max_red = 12
+        max_green = 13
+        max_blue = 14
+        sum_of_possible_game_ids = 0
+        for game in data:
+            game_id = int(game.split(":")[0].replace("Game ", ""))
+            subsets = game.split(":")[1].split(";")
+            max_colors = {
+                "red": 0,
+                "green": 0,
+                "blue": 0,
+            }
+            for subset in subsets:
+                for cube_info in subset.split(","):
+                    logger.debug(cube_info.strip())
+                    number_of_cubes, color = cube_info.strip().split(" ")
+                    if int(number_of_cubes) > max_colors[color]:
+                        max_colors[color] = int(number_of_cubes)
+
+            if (
+                max_colors["red"] <= max_red
+                and max_colors["green"] <= max_green
+                and max_colors["blue"] <= max_blue
+            ):
+                sum_of_possible_game_ids += game_id
+        return sum_of_possible_game_ids
+
+    def part_two():
+        game_powers = []
+        for game in data:
+            game_id = int(game.split(":")[0].replace("Game ", ""))
+            subsets = game.split(":")[1].split(";")
+            max_colors = {
+                "red": 0,
+                "green": 0,
+                "blue": 0,
+            }
+            for subset in subsets:
+                for cube_info in subset.split(","):
+                    logger.debug(cube_info.strip())
+                    number_of_cubes, color = cube_info.strip().split(" ")
+                    if int(number_of_cubes) > max_colors[color]:
+                        max_colors[color] = int(number_of_cubes)
+            game_power = max_colors["red"] * max_colors["green"] * max_colors["blue"]
+            game_powers.append(game_power)
+
+        return sum(game_powers)
+
+    # return part_one()
+    return part_two()
+
+
 if __name__ == "__main__":
-    print(day_1())
+    print(day_2())
